26d5bacc15618e5cac806e0bc2acb4e469e3ce02
nakamura0907 <nakamura990907@gmail.com>
2023-11-06 10:31:27 +0900

    add: BusinessCardPartsCoordinateのORMモデルを作成

--- internal/database/model/model.go
+++ internal/database/model/model.go
@@ -20,7 +20,7 @@ type BusinessCard struct {
 	ID   string `gorm:"primaryKey"`
 	User string
 	// ARAssets
-	// BusinessCardPathsCordinate
+	BusinessCardPathsCoordinate string
 	// BusinessCardBackground
 	BusinessCardName string
 	DisplayName      string
@@ -29,8 +29,33 @@ type BusinessCard struct {
 	OfficialPosition string
 	PhoneNumber      string
 	Email            string
+	PostalCode       string
 	Address          string
 	QRCodeImagePath  string
 	CreatedAt        time.Time
 	AccessCount      int
+}
+
+// BusinessCardPartsCoordinateは名刺パーツ座標テーブルのORMモデルです。
+type BusinessCardPartsCoordinate struct {
+	ID                string         `gorm:"primaryKey"`
+	BusinessCards     []BusinessCard `gorm:"foreignKey:BusinessCardPathsCoordinate"`
+	DisplayNameX      int
+	DisplayNameY      int
+	CompanyNameX      int
+	CompanyNameY      int
+	DepartmentX       int
+	DepartmentY       int
+	OfficialPositionX int
+	OfficialPositionY int
+	PhoneNumberX      int
+	PhoneNumberY      int
+	EmailX            int
+	EmailY            int
+	PostalCodeX       int
+	PostalCodeY       int
+	AddressX          int
+	AddressY          int
+	QRCodeX           int
+	QRCodeY           int
 }
