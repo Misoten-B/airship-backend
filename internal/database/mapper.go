73c7f1a860708d4a79198ddc6e2278ca6785d8d2
nakamura0907 <nakamura990907@gmail.com>
2023-11-06 09:58:34 +0900

    add: ドメイン-モデル変換用ファイルの作成

---
+++ internal/database/mapper.go
@@ -0,0 +1,7 @@
+package database
+
+// ToUserDomainはORMモデルをUser集約に変換します。
+func ToUserDomain() {}
+
+// ToUserModelはUser集約をORMモデルに変換します。
+func ToUserORMModel() {}
--- internal/domain/user/user.go
+++ internal/domain/user/user.go
@@ -2,20 +2,11 @@ package domain
 
 import (
 	"time"
-
-	"github.com/Misoten-B/airship-backend/internal/database/model"
 )
 
 type User struct {
 	id        string
 	createdAt time.Time
-}
-
-func (u *User) toModel() *model.User {
-	return &model.User{
-		ID:        u.id,
-		CreatedAt: u.createdAt,
-	}
 }
 
 func NewUser() *User {
@@ -24,12 +15,5 @@ func NewUser() *User {
 	return &User{
 		id:        "test-id",
 		createdAt: createdAt,
-	}
-}
-
-func fromModel(user *model.User) *User {
-	return &User{
-		id:        user.ID,
-		createdAt: user.CreatedAt,
 	}
 }
