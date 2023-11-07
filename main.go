c64d91d67884203f0452dbcad44bc012b075691d
mitsuYashi <hm20011027@gmail.com>
2023-11-07 14:44:24 +0900

    add: swagger

--- main.go
+++ main.go
@@ -9,10 +9,14 @@ import (
 	_ "github.com/Misoten-B/airship-backend/docs"
 )
 
-// @title           Swagger Example API
+// @title           AIRship API
 // @version         1.0
 // @license.name  Apache 2.0
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+// @host 		  localhost:8080
+// @@securityDefinitions.apikey ApiKeyAuth
+// @in                         header
+// @name                       Authorization
 func main() {
 	r := gin.Default()
 	routes.Register(r)
