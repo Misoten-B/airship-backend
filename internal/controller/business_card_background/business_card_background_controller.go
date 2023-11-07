6aecbd7995b4e755cfcf3bb603d6c51a3f8d0d01
mitsuYashi <hm20011027@gmail.com>
2023-11-08 00:45:23 +0900

    feat: backgrouond

---
+++ internal/controller/business_card_background/business_card_background_controller.go
@@ -0,0 +1,18 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+// @Tags BusinessCardBackground
+// @Router /user/business_card_background [POST]
+// @Security ApiKeyAuth
+// @Param Authorization header string true "Bearer [Firebase JWT Token]"
+// @Param dto.CreateBusinessCardBackgroundRequest body dto.CreateBusinessCardBackgroundRequest true "BusinessCardBackground"
+// @Success 201 {object} nil
+func CreateBusinessCardBackground(_ *gin.Context) {}
+
+// @Tags BusinessCardBackground
+// @Router /user/business_card_background [GET]
+// @Security ApiKeyAuth
+// @Param Authorization header string true "Bearer [Firebase JWT Token]"
+// @Success 201 {object} []dto.BusinessCardBackgroundResponse
+func ReadAllBusinessCardBackground(_ *gin.Context) {}
