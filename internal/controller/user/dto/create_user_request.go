b4d9c2b2db9965134f66f0804fe78899f0e86723
mitsuYashi <hm20011027@gmail.com>
2023-11-07 14:56:31 +0900

    fix: itTouredのみに変更

--- internal/controller/user/dto/create_user_request.go
+++ internal/controller/user/dto/create_user_request.go
@@ -1,7 +1,5 @@
 package dto
 
 type CreateUserRequest struct {
-	RecordedVoicePath *string `json:"recorded_voice_path" example:"url" extensions:"x-nullable"`
-	RecordedModelPath *string `json:"recorded_model_path" example:"url" extensions:"x-nullable"`
-	IsToured          *bool   `json:"is_toured" example:"false" extensions:"x-nullable"`
+	IsToured *bool `json:"is_toured" example:"false" extensions:"x-nullable"`
 }
