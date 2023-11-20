10e3de4023218126359d7d4ddbfdda4d90345dd9
mitsuYashi <hm20011027@gmail.com>
2023-11-20 12:12:56 +0900

    change: rename

--- internal/drivers/firebase.go
+++ internal/drivers/firebase.go
@@ -12,7 +12,7 @@ func InitFirebase() (*firebase.App, error) {
 	opt := option.WithCredentialsFile("/serviceAccountKey.json")
 	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
-		return nil, fmt.Errorf("error initializing app: %v", err)
+		return nil, fmt.Errorf("error initializing app: %w", err)
 	}
 	return app, nil
 }
