a30c601a6753cdbc6fb1f813a2f9ce35e86e17ad
mitsuYashi <hm20011027@gmail.com>
2023-11-21 13:28:40 +0900

    refact: gen getfunc

--- internal/drivers/firebase.go
+++ internal/drivers/firebase.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	firebase "firebase.google.com/go/v4"
+	"firebase.google.com/go/v4/auth"
 	"google.golang.org/api/option"
 )
 
@@ -15,4 +16,16 @@ func InitFirebase() (*firebase.App, error) {
 		return nil, fmt.Errorf("error initializing app: %w", err)
 	}
 	return app, nil
+}
+
+func GetFirebaseAuthClient() (*auth.Client, error) {
+	app, err := InitFirebase()
+	if err != nil {
+		return nil, fmt.Errorf("error initializing app: %w", err)
+	}
+	client, err := app.Auth(context.Background())
+	if err != nil {
+		return nil, fmt.Errorf("error getting Auth client: %w", err)
+	}
+	return client, nil
 }
