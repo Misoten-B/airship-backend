0e8e2a27817ca458b30ad1c774d09458e41a9001
nakamura0907 <nakamura990907@gmail.com>
2023-11-24 09:39:17 +0900

    add: Users生成関数の作成

--- internal/database/mapper/user.go
+++ internal/database/mapper/user.go
@@ -5,18 +5,11 @@ import (
 	domain "github.com/Misoten-B/airship-backend/internal/domain/user"
 )
 
-// ToUserDomainはORMモデルをUser集約に変換します。
-func ToUserDomain(model *model.User) *domain.User {
-	return &domain.User{
-		ID:        model.ID,
-		CreatedAt: model.CreatedAt,
-	}
-}
-
 // ToUserModelはUser集約をORMモデルに変換します。
 func ToUserORMModel(domain *domain.User) *model.User {
 	return &model.User{
-		ID:        domain.ID,
-		CreatedAt: domain.CreatedAt,
+		ID:        domain.ID().String(),
+		CreatedAt: domain.CreatedAt(),
+		DeletedAt: domain.DeletedAt(),
 	}
 }
--- internal/domain/user/user.go
+++ internal/domain/user/user.go
@@ -2,18 +2,39 @@ package domain
 
 import (
 	"time"
+
+	"github.com/Misoten-B/airship-backend/internal/id"
 )
 
 type User struct {
-	ID        string
-	CreatedAt time.Time
+	id        id.ID
+	createdAt time.Time
+	deletedAt time.Time
 }
 
-func NewUser() *User {
+func NewUser() (*User, error) {
 	createdAt := time.Now()
 
-	return &User{
-		ID:        "test-id",
-		CreatedAt: createdAt,
+	id, err := id.NewID()
+	if err != nil {
+		return nil, err
 	}
+
+	return &User{
+		id:        id,
+		createdAt: createdAt,
+		deletedAt: time.Time{}, // zero value
+	}, nil
+}
+
+func (u *User) ID() id.ID {
+	return u.id
+}
+
+func (u *User) CreatedAt() time.Time {
+	return u.createdAt
+}
+
+func (u *User) DeletedAt() time.Time {
+	return u.deletedAt
 }
