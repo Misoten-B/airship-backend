dbb93ef1a52314c2e227a840eca790c2813743d5
mitsuYashi <hm20011027@gmail.com>
2023-11-25 11:18:16 +0900

    feat: 開発環境用guard追加

--- internal/middleware/Guard.go
+++ internal/middleware/Guard.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"firebase.google.com/go/v4/auth"
 	"github.com/gin-gonic/gin"
@@ -10,11 +11,18 @@ import (
 
 func Guard(client *auth.Client) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		if os.Getenv("DEV_MODE") == "true" {
+			log.Println("Development mode - bypassing authentication")
+			c.Next()
+			return
+		}
+
 		idToken := c.GetHeader("Authorization")
 		token, err := client.VerifyIDToken(c.Request.Context(), idToken)
 		if err != nil {
 			log.Printf("error verifying ID token: %v\n", err)
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error: ": "Unauthorized"})
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"code": http.StatusUnauthorized, "error: ": "Unauthorized"})
+			return
 		}
 
 		log.Printf("Verified ID token: %v\n", token)
