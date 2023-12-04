4d6beb25c329c1a692b390371356a4f54a82e7df
mitsuYashi <hm20011027@gmail.com>
2023-12-04 10:12:48 +0900

    add: comment

--- internal/controller/business_card_parts_coordinate/business_card_parts_coordinate_controller.go
+++ internal/controller/business_card_parts_coordinate/business_card_parts_coordinate_controller.go
@@ -13,12 +13,14 @@ import (
 // @Router /v1/business_card_parts_coordinates [GET]
 // @Success 200 {object} []dto.BusinessCardPartsCoordinateResponse
 func ReadAllBusinessCardPartsCoordinate(c *gin.Context) {
+	// データベースに接続
 	db, err := database.ConnectDB()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, err)
 		return
 	}
 
+	// データベースから全ての名刺パーツ座標を取得
 	bcpcs := []model.BusinessCardPartsCoordinate{}
 	result := db.Find(&bcpcs)
 	if result.Error != nil {
@@ -26,6 +28,7 @@ func ReadAllBusinessCardPartsCoordinate(c *gin.Context) {
 		return
 	}
 
+	// レスポンスに変換
 	bcpcr := []dto.BusinessCardPartsCoordinateResponse{}
 	for _, bcpc := range bcpcs {
 		bcpcr = append(bcpcr, dto.ConvertToBusinessCardPartsCoordinateResponse(bcpc))
