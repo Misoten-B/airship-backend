e52ea0194c8bc9deb52e0d211b132b9ad65bcf0c
nakamura0907 <nakamura0907@users.noreply.github.com>
2023-12-08 10:16:25 +0900

    feat: HTTPハンドラ準備部分の実装

--- internal/frameworks/handler/ar_assets/handler.go
+++ internal/frameworks/handler/ar_assets/handler.go
@@ -68,7 +68,6 @@ func CreateArAssets(c *gin.Context) {
 		usecaseImpl = container.InitializeCreateARAssetsUsecaseForProd(db, config)
 	}
 
-	// ユースケース実行
 	input := usecase.ARAssetsCreateInput{
 		UID:                 uid,
 		SpeakingDescription: request.SpeakingDescription,
@@ -90,6 +89,7 @@ func CreateArAssets(c *gin.Context) {
 		return
 	}
 
+	// レスポンス
 	c.Header("Location", fmt.Sprintf("/%s", output.ID))
 	c.JSON(http.StatusCreated, nil)
 }
@@ -101,9 +101,31 @@ func CreateArAssets(c *gin.Context) {
 // @Param ar_assets_id path string true "ArAssets ID"
 // @Success 200 {object} dto.ArAssetsResponse
 func ReadArAssetsByID(c *gin.Context) {
-	log.Print("Authorization: ", c.Request.Header.Get("Authorization"))
-	log.Print("ar_assets_id: ", c.Param("ar_assets_id"))
+	// コンテキストから取得
+	config, err := frameworks.GetConfig(c)
+	if err != nil {
+		frameworks.ErrorHandling(c, err, http.StatusInternalServerError)
+		return
+	}
 
+	uid, err := frameworks.GetUID(c)
+	if err != nil {
+		frameworks.ErrorHandling(c, err, http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("config: %v, uid: %v", config, uid)
+
+	// リクエスト取得
+	id := c.Param("ar_assets_id")
+	if id == "" {
+		reqErr := errors.New("ar_assets_id is empty")
+		frameworks.ErrorHandling(c, reqErr, http.StatusBadRequest)
+	}
+
+	// ユースケース実行
+
+	// レスポンス
 	c.JSON(http.StatusOK, dto.ArAssetsResponse{
 		ID:                   "1",
 		SpeakingDescription:  "こんにちは",
@@ -118,8 +140,25 @@ func ReadArAssetsByID(c *gin.Context) {
 // @Param ar_assets_id path string true "ArAssets ID"
 // @Success 200 {object} dto.ArAssetsResponse
 func ReadArAssetsByIDPublic(c *gin.Context) {
-	log.Print("ar_assets_id: ", c.Param("ar_assets_id"))
+	// コンテキストから取得
+	config, err := frameworks.GetConfig(c)
+	if err != nil {
+		frameworks.ErrorHandling(c, err, http.StatusInternalServerError)
+		return
+	}
 
+	log.Printf("config: %v", config)
+
+	// リクエスト取得
+	id := c.Param("ar_assets_id")
+	if id == "" {
+		reqErr := errors.New("ar_assets_id is empty")
+		frameworks.ErrorHandling(c, reqErr, http.StatusBadRequest)
+	}
+
+	// ユースケース実行
+
+	// レスポンス
 	c.JSON(http.StatusOK, dto.ArAssetsResponse{
 		ID:                   "1",
 		SpeakingDescription:  "こんにちは",
@@ -135,8 +174,26 @@ func ReadArAssetsByIDPublic(c *gin.Context) {
 // @Param Authorization header string true "Bearer [Firebase JWT Token]"
 // @Success 200 {object} []dto.ArAssetsResponse
 func ReadAllArAssets(c *gin.Context) {
-	log.Printf("Authorization: %s", c.Request.Header.Get("Authorization"))
+	// コンテキストから取得
+	config, err := frameworks.GetConfig(c)
+	if err != nil {
+		frameworks.ErrorHandling(c, err, http.StatusInternalServerError)
+		return
+	}
 
+	uid, err := frameworks.GetUID(c)
+	if err != nil {
+		frameworks.ErrorHandling(c, err, http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("config: %v, uid: %v", config, uid)
+
+	// リクエスト取得
+
+	// ユースケース実行
+
+	// レスポンス
 	c.JSON(http.StatusOK, []dto.ArAssetsResponse{
 		{
 			ID:                   "1",
@@ -158,15 +215,38 @@ func ReadAllArAssets(c *gin.Context) {
 // @Param qrcodeIcon formData file false "Image file to be uploaded"
 // @Success 200 {object} dto.ArAssetsResponse
 func UpdateArAssets(c *gin.Context) {
-	log.Printf("Authorization: %s", c.Request.Header.Get("Authorization"))
+	/// コンテキストから取得
+	config, err := frameworks.GetConfig(c)
+	if err != nil {
+		frameworks.ErrorHandling(c, err, http.StatusInternalServerError)
+		return
+	}
+
+	uid, err := frameworks.GetUID(c)
+	if err != nil {
+		frameworks.ErrorHandling(c, err, http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("config: %v, uid: %v", config, uid)
+
+	// リクエスト取得
+	id := c.Param("ar_assets_id")
+	if id == "" {
+		reqErr := errors.New("ar_assets_id is empty")
+		frameworks.ErrorHandling(c, reqErr, http.StatusBadRequest)
+	}
 
 	request := dto.CreateArAssetsRequest{}
-	if err := c.ShouldBind(&request); err != nil {
+	if err = c.ShouldBind(&request); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	log.Printf("formData: %v", request)
 
+	// ユースケース実行
+
+	// レスポンス
 	c.JSON(http.StatusCreated, dto.ArAssetsResponse{
 		ID:                   "1",
 		SpeakingDescription:  "こんにちは",
@@ -183,9 +263,31 @@ func UpdateArAssets(c *gin.Context) {
 // @Param ar_assets_id path string true "ArAssets ID"
 // @Success 204 {object} nil
 func DeleteArAssets(c *gin.Context) {
-	log.Printf("Authorization: %s", c.Request.Header.Get("Authorization"))
-	log.Printf("ar_assets_id: %s", c.Param("ar_assets_id"))
+	// コンテキストから取得
+	config, err := frameworks.GetConfig(c)
+	if err != nil {
+		frameworks.ErrorHandling(c, err, http.StatusInternalServerError)
+		return
+	}
 
+	uid, err := frameworks.GetUID(c)
+	if err != nil {
+		frameworks.ErrorHandling(c, err, http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("config: %v, uid: %v", config, uid)
+
+	// リクエスト取得
+	id := c.Param("ar_assets_id")
+	if id == "" {
+		reqErr := errors.New("ar_assets_id is empty")
+		frameworks.ErrorHandling(c, reqErr, http.StatusBadRequest)
+	}
+
+	// ユースケース実行
+
+	// レスポンス
 	c.JSON(http.StatusNoContent, nil)
 }
 
