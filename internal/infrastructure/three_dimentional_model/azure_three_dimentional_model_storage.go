2a513245fb8e8b695b26b09a4f3cfe16982f6200
nakamura0907 <nakamura0907@users.noreply.github.com>
2023-12-08 20:34:58 +0900

    feat: Azure3Dモデルストレージ実装、動作未確認

---
+++ internal/infrastructure/three_dimentional_model/azure_three_dimentional_model_storage.go
@@ -0,0 +1,47 @@
+package threedimentionalmodel
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/sas"
+)
+
+type AzureThreeDimentionalModelStorage struct {
+	connectionString string
+}
+
+const (
+	sasExpiryDuration = 24 * time.Hour
+	containerName     = "three-dimentional-models"
+)
+
+func NewAzureThreeDimentionalModelStorage(connectionString string) *AzureThreeDimentionalModelStorage {
+	return &AzureThreeDimentionalModelStorage{
+		connectionString: connectionString,
+	}
+}
+
+func (s *AzureThreeDimentionalModelStorage) GetModelURL(modelName string) (string, error) {
+	serviceClient, err := azblob.NewClientFromConnectionString(s.connectionString, nil)
+	if err != nil {
+		return "", fmt.Errorf("failed to create service client: %w", err)
+	}
+
+	blobClient := serviceClient.ServiceClient().
+		NewContainerClient(containerName).
+		NewBlobClient(modelName)
+
+	permissions := sas.BlobPermissions{
+		Read: true,
+	}
+	expiry := time.Now().Add(sasExpiryDuration)
+
+	url, err := blobClient.GetSASURL(permissions, expiry, nil)
+	if err != nil {
+		return "", fmt.Errorf("failed to get SAS URL: %w", err)
+	}
+
+	return url, nil
+}
