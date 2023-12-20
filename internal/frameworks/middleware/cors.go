86e0abd10cbd075e5bac252ab575e6578e47ab94
nakamura0907 <nakamura0907@users.noreply.github.com>
2023-12-20 15:44:56 +0900

    fix: CORSのクライアント側に発生したバグ解消

--- internal/frameworks/middleware/cors.go
+++ internal/frameworks/middleware/cors.go
@@ -20,7 +20,7 @@ func CORSMiddleware() gin.HandlerFunc {
 		c.Writer.Header().Set(
 			"Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization",
 		)
-		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length, Location")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if c.Request.Method == http.MethodOptions {
